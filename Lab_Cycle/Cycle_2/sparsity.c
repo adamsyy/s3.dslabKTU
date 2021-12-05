4116e72bb05b422fb085d17eedb57a0301d7ebbf
adamsy <adamoommenjacob@gmail.com>
2021-12-05 21:06:38 +0530

    sparse

--- Lab_Cycle/Cycle_2/sparsity.c
+++ Lab_Cycle/Cycle_2/sparsity.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
+typedef struct{
+    int row;
+    int col;
+    int value;
+}term;
+ 
+
+
 
 void main(){
-    int a[3]={62,24,73};
-    int p=3;
-    printf("%d",p);
-   testahne(&p);
-   printf("%d",p);
+   int size;
+     printf("enter no of non zero elements");
+     scanf("%d",&size);
+     term a[size];
+     a[0].value=size;
+  printf("enter number of row and col");
+  scanf("%d%d",&a[0].row,&a[0].col);
+   
+  for(int i=1;i<=a[0].value;i++){
+       printf("enter %dth non zero element in the format row,col,value",i);
+    scanf("%d%d%d",&a[i].row,&a[i].col,&a[i].value);
+  }
+
 }
 
-void testahne(int* arr){
-*arr=*arr+1;
-}
 
 
