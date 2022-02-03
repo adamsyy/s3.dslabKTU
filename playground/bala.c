f5530326be317868d2672b7fb4525f512600bb7f
adamsyy <adamoommenjacob@gmail.com>
2022-02-03 13:42:45 +0530

    balanced

--- playground/bala.c
+++ playground/bala.c
@@ -1,35 +1,80 @@
+char peek();
+void push(char);
+char pop();
+#include<string.h>
 #include<stdio.h>
 char arr[101];
+int check=0;
 int top=-1;
 
 
 void main(){
- 
-push('3');
-push('2');
-push('5');
-display();
-}
+  //printf("%d",top);
+push('( ');
+ //printf("%d",top);
+push('(');
+ //printf("%d",top);
+push(')');
+ //printf("%d",top);
+push('(');
+push(')');
+push(')');
+push(')');
+    //printf("%d",top);
 
+if(top==-1&&check==0){ 
+    printf("balanced");}
+   else{
+printf("not balanced");
+   } 
+
+}
+char peek(){
+     if(top==-1){ check=1;
+        return 'p';}
+    
+return arr[top];
+}
+char pop(){
+    if(top==-1){ return -1;}
+return arr[top--];
+//if(arr[top--]== )
+}
 void push(char item){
     if(top==100){
+        check=1;
         printf("full ai");
         return;
     }
     //arr[++top]=item;
     if(item=='('||item=='{'||item=='['){
         arr[++top]=item;
+        
     }
+    if(item==')'&&peek()=='('){
+    pop();
+    }
+    if(item=='}'&&peek()=='{'){
+    pop();
+    }
+    if(item==']'&&peek()=='['){
+    pop();
+    }
+
 }
 
-int pop(){
-    if(top==-1){printf("underflow"); return;}
-//return arr[top--];
-if(arr[top--]==)
-}
 
-void display(){
-    for(int i=top;i>=0;i--){
-        printf("%c",arr[i]);
-    }
-}
+
+
+
+// void checkin(){
+//     for(int i=top;i>=0;i--){
+// if()
+//     }
+// }
+
+// void display(){
+//     for(int i=top;i>=0;i--){
+//         printf("%c",arr[i]);
+//     }
+// }
