a1f1090b16d444326312693095ecb70340e676dc
adamsyy <adamoommenjacob@gmail.com>
2022-02-21 00:45:44 +0530

    queue problems

--- playground/solution.c
+++ playground/solution.c
@@ -1,99 +1,110 @@
-//CSL201 DATA STRUCTURES LAB ----- VISHRUTH S, CS3A, 61
-//CYCLE 2 QUESTION 16
-//To evaluate a given postfix expression 
+// //CSL201 DATA STRUCTURES LAB ----- VISHRUTH S, CS3A, 61
+// //CYCLE 2 QUESTION 20
+// //PROGRAM TO ADD TWO POLYNOMIALS USING ARRAY
 
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <math.h>
+// #include <stdio.h>
 
-// ===== RESULT STACK ======== //
-int stack[100];
-int top = -1;
+// void inputPoly(int, int[2][100]);
+// void printPoly(int, int[2][100]);
 
-// ===== STACK FUNCTIONS ===== //
-void stack_push(int el)
-{
-    stack[++top] = el;
-}
+// // ====== FUNCTION TO ADD 2 POLYNOMIALS ====== //
+// // Polynomial is a 2d array, first row contains powers, second row contains coefficients
+// void polyAdd(int poly1[2][100], int poly2[2][100], int size1, int size2)
+// {
+//   int sum[2][100], size3;
+//   int i = 0, j = 0, k = 0;
+//   while (i < size1 && j < size2)
+//   {
+//     if (poly1[0][i] == poly2[0][j]) // If powers are same
+//     {
+//       sum[0][k] = poly1[0][i];
+//       sum[1][k] = poly1[1][i] + poly2[1][j]; // Then coefficients are added
+//       i++;
+//       j++;
+//     }
+//     else if (poly1[0][i] > poly2[0][j]) // If power of first > second
+//     {
+//       sum[0][k] = poly1[0][i];
+//       sum[1][k] = poly1[1][i]; // Then it is copied
+//       i++;
+//     }
+//     else
+//     {
+//       sum[0][k] = poly2[0][j];
+//       sum[1][k] = poly2[1][j]; // else, second one is copied. This is to maintian the descending order of powers in sum
+//       j++;
+//     }
+//     k++;
+//   }
+//   while (i < size1) // Copy remaining elements in first polynomial, if any
+//   {
+//     sum[0][k] = poly1[0][i];
+//     sum[1][k] = poly1[1][i];
+//     i++;
+//     k++;
+//   }
+//   while (j < size2) // Copy remaining elements in second polynomial, if any
+//   {
+//     sum[0][k] = poly2[0][j];
+//     sum[1][k] = poly2[1][j];
+//     j++;
+//     k++;
+//   }
+//   size3 = k;
 
-int stack_pop()
-{
-    return stack[top--];
-}
+//   printf("\nSum of Polynomials: ");
+//   printPoly(size3, sum);
+// }
 
-int stack_peek()
-{
-    return stack[top];
-}
+// // Main function
+// int main()
+// {
 
-// ====== OTHER FUNCTIONS ===== //
+//   int poly1[2][100], poly2[2][100];
+//   int size1, size2;
+//   printf("\nEnter number of terms in first polynomial  ");
+//   scanf("%d", &size1);
+//   if (size1 < 0 || size1 > 100)
+//   {
+//     printf("\nInvalid size or size exceeded max limit (100) ");
+//     return 0;
+//   }
+//   inputPoly(size1, poly1);
+//   printPoly(su)
+//   // printf("\nEnter number of terms in second polynomial  ");
+//   // scanf("%d", &size2);
+//   // if (size2 < 0 || size2 > 100)
+//   // {
+//   //     printf("\nInvalid size or size exceeded max limit (100) ");
+//   //     return 0;
+//   // }
+//   // inputPoly(size2, poly2);
 
-// To check if current character is digit
-bool isDigit(char c)
-{
-    return (c >= '0' && c <= '9');
-}
+//   // printf("\nFirst Polynomial: ");
+//   // printPoly(size1, poly1);
+//   // printf("\nSecond Polynomial: ");
+//   // printPoly(size2, poly2);
+//   // polyAdd(poly1, poly2, size1, size2);
+// }
 
-// To perform the given operation on operands and return result
-int performOperation(char operator, int op1, int op2)
-{
-    switch (operator)
-    {
-    case '+':
-        return op2 + op1;
-    case '-':
-        return op2 - op1;
-    case '*':
-        return op2 * op1;
-    case '/':
-        return op2 / op1;
-    case '^':
-        return pow(op2, op1);
-    default:
-        return -1;
-    }
-}
+// // Function to input a polynomial
+// void inputPoly(int size, int poly[2][100])
+// {
+//   printf("\nEnter the coeffiecent and power of polynomial, in descending order of power");
+//   printf("\nFor example: 3X^(5) + 4x^(2) + x + 6 is entered as 3 5 4 2 1 1 6 0\n");
+//   for (int i = 0; i < size; i++)
+//   {
+//     scanf("%d", &poly[1][i]);
+//     scanf("%d", &poly[0][i]);
+//   }
+// }
 
-// ===== POSTFIX EVALUATION FUNCTION ===== //
-int postfixEval(char exp[])
-{
-    int n = strlen(exp);        // length of given expression
-    for (int i = 0; i < n; i++) // for each character in expression
-    {
-        if (exp[i] == ' ')   // if character is space
-            continue;        // then continue to next character
-        if (isDigit(exp[i])) // if character is digit
-        {
-            int num = 0;
-            while (isDigit(exp[i])) // To continue in case of multiple digits
-            {
-                num = num * 10 + (int)(exp[i] - '0'); // '0' is subtracted since exp[i] is a character
-                i++;
-            }
-            i--;
-            stack_push(num); // the resulting number is pushed to the stack
-        }
-        else // If operator
-        {
-            int op1 = stack_pop(); // Then pop two elements from stack
-            int op2 = stack_pop();
-            int result = performOperation(exp[i], op1, op2); // and perform the operation on them
-            stack_push(result);                              // Push this result to the stack
-        }
-    }
-    return stack_peek(); // The answer will be the top and only element remaining in stack
-}
-
-// ====== MAIN FUNCTION ==== //
-int main()
-{
-    char exp[100]; // postfix expression
-    printf("\nEnter postfix expression, each item must be space separated\n");
-    printf("For example: 12 32 + 44 /\n");
-    gets(exp);
-    int EvaluatedValue = postfixEval(exp);
-    printf("\nResult Of evaluated postfix:  %d", EvaluatedValue);
-
-    return 0;
-}
+// // Function to print polynomial
+// void printPoly(int size, int poly[2][100])
+// {
+//   for (int i = 0; i < size; i++)
+//   {
+//     printf(" %dx^(%d) ", poly[1][i], poly[0][i]);
+//     i < size - 1 ? printf("+") : printf("");
+//   }
+// }
